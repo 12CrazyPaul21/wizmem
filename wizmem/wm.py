a0ba5e543baeaca79d9d1f7704e178030ce51147
phantom <604916833@qq.com>
2022-07-02 19:29:40 +0800

    feat(program): 增加程序托盘以方便进入Web界面和退出服务

--- wizmem/wm.py
+++ wizmem/wm.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
 
 import threading, webbrowser, socket, errno, sys
+import pystray
+import urllib.request
+
+from PIL import Image
 
 from wizmem import web
+
+WIZMEM_SERVER = 'localhost'
+WIZMEM_SERVER_PORT = 7379
+WIZMEM_SERVER_URL = "http://%s:%d" % (WIZMEM_SERVER, WIZMEM_SERVER_PORT)
 
 def check_singleton(server, port):
     result = False
@@ -16,15 +24,31 @@ def check_singleton(server, port):
 
     return result
 
+def setup_systemtray():
+    icon_path = 'static/favicon.ico'
+    if not getattr(sys, 'frozen', False):
+        icon_path = 'wizmem/%s' % icon_path
+
+    menu = pystray.Menu(
+        pystray.MenuItem(
+            text='Open Web Manager Site',
+            action=lambda: webbrowser.open(WIZMEM_SERVER_URL)
+        ),
+        pystray.MenuItem(
+            text='Quit',
+            action=lambda: urllib.request.urlopen('%s/shutdown' % WIZMEM_SERVER_URL)
+        ),
+    )
+
+    icon = pystray.Icon('wizmem', icon=Image.open(icon_path), menu=menu)
+    threading.Thread(target=lambda: icon.run(), daemon=True).start()
 
 def run():
-    server = 'localhost'
-    port = 7379
-    url = "http://%s:%d" % (server, port)
+    if check_singleton(WIZMEM_SERVER, WIZMEM_SERVER_PORT):
+        webbrowser.open(WIZMEM_SERVER_URL)
+        sys.exit(0)
 
-    if check_singleton(server, port):
-        webbrowser.open(url)
-        sys.exit(0)        
+    setup_systemtray()
 
-    threading.Timer(1.5, lambda : webbrowser.open(url)).start()
-    web.app.build(__name__).run(port=port)
+    threading.Timer(1.5, lambda: webbrowser.open(WIZMEM_SERVER_URL)).start()
+    web.app.build(__name__).run(port=WIZMEM_SERVER_PORT)
