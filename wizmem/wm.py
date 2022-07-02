0bce4755dc617b0a7abefcadeaa9804171cf0a62
phantom <604916833@qq.com>
2022-07-02 17:52:37 +0800

    feat(program): 加入限定wizmem进程单例运行

--- wizmem/wm.py
+++ wizmem/wm.py
@@ -1,11 +1,30 @@
 #!/usr/bin/env python
 
-import threading, webbrowser
+import threading, webbrowser, socket, errno, sys
 
 from wizmem import web
 
+def check_singleton(server, port):
+    result = False
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        try:
+            sock.bind((server, port))
+        except socket.error as e:
+            if e.errno == errno.EADDRINUSE:
+                result = True
+
+    return result
+
+
 def run():
+    server = 'localhost'
     port = 7379
-    url = "http://127.0.0.1:%d" % port
+    url = "http://%s:%d" % (server, port)
+
+    if check_singleton(server, port):
+        webbrowser.open(url)
+        sys.exit(0)        
+
     threading.Timer(1.5, lambda : webbrowser.open(url)).start()
     web.app.build(__name__).run(port=port)
